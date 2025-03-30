e9a39f8ee63bf49cbb21c0c1bd1a29cec66699f2
WhittJS <whittsanders@gmail.com>
2025-03-30 12:25:38 -0400

    tokenizer creation

---
+++ ifstmt-predictor.py
@@ -0,0 +1,14 @@
+from transformers import T5ForConditionalGeneration, AutoModelForSeq2SeqLM
+from transformers import RobertaTokenizer
+from datasets import DatasetDict
+from transformers import TrainingArguments, Trainer
+from transformers import EarlyStoppingCallback
+
+model_checkpoint = "Salesforce/codet5-small"
+
+model = T5ForConditionalGeneration.from_pretrained(model_checkpoint)
+
+tokenizer = RobertaTokenizer.from_pretrained(model_checkpoint)
+tokenizer.add_tokens(["<IF-STMT>"])
+
+model.resize_token_embeddings(len(tokenizer))
