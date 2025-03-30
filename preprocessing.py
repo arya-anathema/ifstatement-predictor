574e6ef4e98fd582356a3c9a379072939eb11818
arya-anathema <183879173+arya-anathema@users.noreply.github.com>
2025-03-30 15:04:45 -0400

    Working preprocessor + preprocessed CSV files

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,8 @@
 import sys
 import pandas as pd
 import io
+import re
+from tqdm import tqdm
 
 if(len(sys.argv) == 1):
     print("Please provide a filepath")
@@ -11,10 +13,25 @@ with open(sys.argv[1],'r') as f:
 df = pd.read_csv(io.StringIO(data))
 
 targets = df['target_block'].to_list()
+for i in range(len(targets)):
+    targets[i] = targets[i].replace(' ','')
 
-for i in range(len(df)):
+def mask_replace(i,input):
+    regex = re.compile(' *'.join(map(re.escape, targets[i])))
+    substring = regex.search(input)
+    if substring:
+        return input.replace(input[substring.start():substring.end()],'<IF-STMT>',1)
+    else:
+        print("Substring not found :(")
+
+for i in tqdm(range(len(df))):
+    df.at[i,'cleaned_method'] = mask_replace(i,df.at[i,'cleaned_method'])
     df.at[i,'cleaned_method'] = df.at[i,'cleaned_method'].replace('\n','')
-    #df.at[i,'cleaned_method'] = df.at[i,'cleaned_method'].replace(targets[i],'<mask>',1)
     df.at[i,'cleaned_method'] = df.at[i,'cleaned_method'].replace('    ','<tab>')
 
-df.to_csv("output.csv")
+output_filename = 'output.csv'
+if(sys.argv[1] == '.\\Archive\\ft_test.csv'): output_filename = 'test_output.csv'
+if(sys.argv[1] == '.\\Archive\\ft_train.csv'): output_filename = 'train_output.csv'
+if(sys.argv[1] == '.\\Archive\\ft_valid.csv'): output_filename = 'valid_output.csv'
+
+df.to_csv(output_filename)
